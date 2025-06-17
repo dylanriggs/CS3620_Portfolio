0dd271ed21b366c9bea54c3538a56abcb05db6e8
dylanriggs <156150661+dylanriggs@users.noreply.github.com>
2025-06-16 19:15:22 -0600

    Mod 5 Tutorial

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://th.bing.com/th/id/OIP.Ygxe4M39-El0f3HPLQOmWQHaHa?w=191&h=191&c=7&r=0&o=7&pid=1.7&rm=3")
--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
 
+app_name = 'food'
 urlpatterns = [
+    path('<int:item_id>', views.detail, name="detail"),
     path('', views.index, name="index"),
     path('item/', views.item, name="item"),
 ]
