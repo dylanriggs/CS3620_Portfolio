8923c1738503d440906f402920a81a817a75c3ce
dylanriggs <156150661+dylanriggs@users.noreply.github.com>
2025-07-06 14:51:11 -0600

    Mod7_Tutorial

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 
 app_name = 'food'
 urlpatterns = [
-    path('<int:item_id>', views.detail, name="detail"),
-    path('', views.index, name="index"),
+    path('<int:pk>', views.FoodDetail.as_view(), name="detail"),
+    path('', views.IndexClassView.as_view(), name="index"),
     path('item/', views.item, name="item"),
-    path('add', views.create_item, name="create_item"),
+    path('add', views.CreateItem.as_view(), name="create_item"),
     path('update/<int:id>', views.update_item, name="update_item"),
     path('delete/<int:id>', views.delete_item, name="delete_item"),
 ]
